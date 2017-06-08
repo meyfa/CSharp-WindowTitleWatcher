400159e353c40f25385b8669a84c65b1df55e610
JangoBrick <mail@jangobrick.de>
2017-06-08 18:31:06 +0200

    Add Watcher title getter, constructor from Process

--- src/Watcher.cs
+++ src/Watcher.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WindowTitleWatcher
 {
     public class Watcher
     {
+        private Process process;
+        
+        /// <summary>
+        /// Gets the current window title.
+        /// </summary>
+        private string Title
+        {
+            get
+            {
+                return process.HasExited ? null : process.MainWindowTitle;
+            }
+        }
+
+        public Watcher(Process proc)
+        {
+            process = proc;
+        }
     }
 }
