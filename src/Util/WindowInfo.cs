98488017e240f467d335bdc8740c97d75ec2c972
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2018-08-01 14:50:45 +0200

    Revert "out uint pid" once again

--- src/Util/WindowInfo.cs
+++ src/Util/WindowInfo.cs
@@ -27,7 +27,9 @@ namespace WindowTitleWatcher.Util
         {
             get
             {
-                GetWindowThreadProcessId(Handle, out uint pid);
+                uint pid;
+                GetWindowThreadProcessId(Handle, out pid);
+
                 return (int)pid;
             }
         }
